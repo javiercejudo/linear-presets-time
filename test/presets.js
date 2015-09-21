235716878f6f20446579d53cc3d2c5c2fdfd30a9
Javier Cejudo <javier@javiercejudo.com>
2015-09-21 23:58:19 +1000

    feat: modify presets schema

--- test/presets.js
+++ test/presets.js
@@ -4,11 +4,12 @@
 
 require('should');
 
-var rescale = require('rescale')(require('floating-adapter')).rescale;
-var time = require('../src/linear-presets-time');
+var Decimal = require('linear-arbitrary-precision')(require('floating-adapter'));
+var rescale = require('rescale')(Decimal).rescale;
+var time = require('linear-preset-factory')(require('../src/linear-presets-time'));
 
 function convert(x, preset) {
-  return rescale(x, preset[0], preset[1]);
+  return Number(rescale(x, preset[0], preset[1]));
 };
 
 function invert(preset) {
@@ -17,32 +18,32 @@ function invert(preset) {
 
 describe('time presets', function() {
   it('should convert correctly', function() {
-    (259200).should.be.exactly(convert(259200000000000, invert(time.secondToNanosecond)), 'secondToNanosecond')
-      .and.exactly(convert(259200000000, invert(time.secondToMicrosecond)), 'secondToMicrosecond')
-      .and.exactly(convert(259200000000, invert(time.secondToMicrosecond)), 'secondToMicrosecond')
-      .and.exactly(convert(259200000, invert(time.secondToMillisecond)), 'secondToMillisecond')
-      .and.exactly(convert(4320, invert(time.secondToMinute)), 'secondToMinute')
-      .and.exactly(convert(72, invert(time.secondToHour)), 'secondToHour')
-      .and.exactly(convert(3, invert(time.secondToDay)), 'secondToDay')
-      .and.exactly(convert(0.42857142857142856, invert(time.secondToWeek)), 'secondToWeek')
-      .and.approximately(convert(0.09856465225158627, invert(time.secondToMonth)), 10e-11, 'secondToMonth')
-      .and.exactly(convert(0.008213721020965523, invert(time.secondToYear)), 'secondToYear')
-      .and.exactly(convert(0.0008213721020965523, invert(time.secondToDecade)), 'secondToDecade')
-      .and.exactly(convert(0.00008213721020965523, invert(time.secondToCentury)), 'secondToCentury')
-      .and.approximately(convert(0.000008213721020965522, invert(time.secondToMillennium)), 10e-11, 'secondToMillennium');
+    (259200).should.be.exactly(convert(259200000000000, invert(time.second_nanosecond)), 'second_nanosecond')
+      .and.exactly(convert(259200000000, invert(time.second_microsecond)), 'second_microsecond')
+      .and.exactly(convert(259200000000, invert(time.second_microsecond)), 'second_microsecond')
+      .and.exactly(convert(259200000, invert(time.second_millisecond)), 'second_millisecond')
+      .and.exactly(convert(4320, invert(time.second_minute)), 'second_minute')
+      .and.exactly(convert(72, invert(time.second_hour)), 'second_hour')
+      .and.exactly(convert(3, invert(time.second_day)), 'second_day')
+      .and.exactly(convert(0.42857142857142856, invert(time.second_week)), 'second_week')
+      .and.approximately(convert(0.09856465225158627, invert(time.second_month)), 10e-11, 'second_month')
+      .and.exactly(convert(0.008213721020965523, invert(time.second_year)), 'second_year')
+      .and.exactly(convert(0.0008213721020965523, invert(time.second_decade)), 'second_decade')
+      .and.exactly(convert(0.00008213721020965523, invert(time.second_century)), 'second_century')
+      .and.approximately(convert(0.000008213721020965522, invert(time.second_millennium)), 10e-11, 'second_millennium');
 
-    (0).should.be.exactly(convert(0, time.secondToNanosecond), 'secondToNanosecond')
-      .and.exactly(convert(0, time.secondToMicrosecond), 'secondToMicrosecond')
-      .and.exactly(convert(0, time.secondToMicrosecond), 'secondToMicrosecond')
-      .and.exactly(convert(0, time.secondToMillisecond), 'secondToMillisecond')
-      .and.exactly(convert(0, time.secondToMinute), 'secondToMinute')
-      .and.exactly(convert(0, time.secondToHour), 'secondToHour')
-      .and.exactly(convert(0, time.secondToDay), 'secondToDay')
-      .and.exactly(convert(0, time.secondToWeek), 'secondToWeek')
-      .and.exactly(convert(0, time.secondToMonth), 'secondToMonth')
-      .and.exactly(convert(0, time.secondToYear), 'secondToYear')
-      .and.exactly(convert(0, time.secondToDecade), 'secondToDecade')
-      .and.exactly(convert(0, time.secondToCentury), 'secondToCentury')
-      .and.exactly(convert(0, time.secondToMillennium), 'secondToMillennium');
+    (0).should.be.exactly(convert(0, time.second_nanosecond), 'second_nanosecond')
+      .and.exactly(convert(0, time.second_microsecond), 'second_microsecond')
+      .and.exactly(convert(0, time.second_microsecond), 'second_microsecond')
+      .and.exactly(convert(0, time.second_millisecond), 'second_millisecond')
+      .and.exactly(convert(0, time.second_minute), 'second_minute')
+      .and.exactly(convert(0, time.second_hour), 'second_hour')
+      .and.exactly(convert(0, time.second_day), 'second_day')
+      .and.exactly(convert(0, time.second_week), 'second_week')
+      .and.exactly(convert(0, time.second_month), 'second_month')
+      .and.exactly(convert(0, time.second_year), 'second_year')
+      .and.exactly(convert(0, time.second_decade), 'second_decade')
+      .and.exactly(convert(0, time.second_century), 'second_century')
+      .and.exactly(convert(0, time.second_millennium), 'second_millennium');
   });
 });
